4e12bcba894a92ee0015523887b0f2fc15f172b8
Mikolaj <samulak.mikolaj@outlook.com>
2022-01-16 20:53:25 +0100

    update TemperatureF in Service1

--- Mikolaj.Samulak.Service1/WeatherForecast.cs
+++ Mikolaj.Samulak.Service1/WeatherForecast.cs
@@ -8,7 +8,7 @@ namespace Mikolaj.Samulak.Service1
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC * 1.8);
 
         public string Summary { get; set; }
     }
